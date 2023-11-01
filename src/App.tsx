53b803ec238b12b1323bc6e8d915a2cde54c2679
Xiaoxia Chen <xiaoxia.chen@comptel.com>
2023-11-01 11:50:30 +0200

    refactor it to be more modular

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Channel, Feed, ApiResponse } from './types';
 import Headline from './Headline';
-import './percent.css';
-
+import PercentBar from './PercentBar';
 
 const App: React.FC = () => {
   const [moistureLevel, setMoistureLevel] = useState<number | null>(null);
@@ -10,7 +9,7 @@ const App: React.FC = () => {
 
   const minMoisture = 0.4;
   const maxMoisture = 0.65;
-  const percentContainerHeight = 350; // 350px
+  const percentContainerHeight = 350; // 350px is the height of the percentage component
 
 
   const fetchData = () => {
@@ -57,15 +56,12 @@ const App: React.FC = () => {
         <div className="data-container">
           <Headline alarmClassName={`alarm-icon ${determineBarClass()}`}></Headline>
 
-          <div className="percentage-bar-container">
-            <div className="ideal-percentage-bar-filled" style={{ top: idealTop, height: idealHeight }}>
-              <div className="ideal-percentage-bar-strikes">
-                <div className="ideal-percentage-bar-strikes-instruction">Optimal Moisture Level</div>
-              </div>
-            </div>
-            <div className={`percentage-bar-filled ${determineBarClass()}`} style={{ height: moistureLevel*percentContainerHeight }}></div>
-            <div className="percentage-value">{(moistureLevel*100).toFixed(2)}%</div>
-          </div>
+          <PercentBar
+            moistureLevel={moistureLevel}
+            minMoisture={minMoisture}
+            maxMoisture={maxMoisture}
+            percentContainerHeight={percentContainerHeight}
+          />
         </div>
       ) : (
         'Loading...'
--- src/percentbar.tsx
+++ src/percentbar.tsx
@@ -1,22 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import './percent.css'; // Make sure to import the stylesheet
+import React from 'react';
+import './PercentBar.css'; // Make sure to import the stylesheet
 
-const PercentBar: React.FC = () => {
-  const [percent, setPercent] = useState(0);
+interface PercentBarProps {
+  moistureLevel: number;
+  minMoisture: number;
+  maxMoisture: number;
+  percentContainerHeight: number;
+}
 
-  // Simulate percent changing over time; you can replace this with your logic
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setPercent(prevPercent => (prevPercent + 0.1) % 1);
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
+const PercentBar: React.FC<PercentBarProps> = ({ moistureLevel, minMoisture, maxMoisture, percentContainerHeight }) => {
+  const determineBarClass = () => {
+    if (moistureLevel < minMoisture) return 'status-red';
+    if (moistureLevel > maxMoisture) return 'status-orange';
+    return 'status-green';
+  };
 
-  const filledHeight = `${percent * 100}%`;
+  const idealTop = `${minMoisture * 100}%`;
+  const idealHeight = `${(maxMoisture - minMoisture) * 100}%`;
 
   return (
     <div className="percentage-bar-container">
-      <div className="percentage-bar-filled" style={{ height: filledHeight }}></div>
+      <div className="ideal-percentage-bar-filled" style={{ top: idealTop, height: idealHeight }}>
+        <div className="ideal-percentage-bar-strikes">
+          <div className="ideal-percentage-bar-strikes-instruction">Optimal Moisture Level</div>
+        </div>
+      </div>
+      <div className={`percentage-bar-filled ${determineBarClass()}`} style={{ height: moistureLevel*percentContainerHeight }}></div>
+      <div className="percentage-value">{(moistureLevel*100).toFixed(2)}%</div>
     </div>
   );
 };
